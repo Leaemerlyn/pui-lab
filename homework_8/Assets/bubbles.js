d9f8b219f2a959e2031a7578c80571ac2412ac01
Lea Emerlyn <leaemerlyn@Leas-MacBook-Pro.local>
2020-11-25 15:05:35 -0500

    bubble effect

--- homework_8/Assets/bubbles.js
+++ homework_8/Assets/bubbles.js
@@ -10,6 +10,9 @@ function makeBubble(){
 
 function updateBubbles(){
     currentBubbleFrames ++;
+    for (var i=0; i<bubbles.length; i++){
+        pushBubble(bubbles[i])
+    }
     if (currentBubbleFrames >= bubbleFrames){
         makeBubble();
         currentBubbleFrames = 0;
@@ -19,17 +22,44 @@ function updateBubbles(){
     }
 }
 
-function cssAdd( input, sum )
-{
+function pushBubble(bubble){
+    let xOffset = bubble.getX() - mouseX;
+    let yOffset = bubble.getY() - mouseY;
+    let squareDist = xOffset**2 + yOffset**2;
+    let dist = Math.sqrt(squareDist);
+    xOffset = xOffset/dist;
+    yOffset = yOffset/dist;
+    if (dist < 500){
+        bubble.xPush = xOffset * 200/dist;
+        bubble.yPush = yOffset *200/dist;
+    }
+    else{
+        bubble.xPush = 0;
+        bubble.yPush = 0;
+    }
+}
+
+function cssAdd( input, sum ){
     return (parseInt(input) + sum).toString() + "px";
+}
+
+let mouseX = 0;
+let mouseY = 0;
+
+document.body.onmouseenter = function(e){
+    mouseX = e.clientX + document.documentElement.scrollLeft;
+    mouseY = e.clientY + document.documentElement.scrollTop;
+}
+
+document.body.onmousemove = function(e){
+    mouseX = e.clientX + document.documentElement.scrollLeft;
+    mouseY = e.clientY + document.documentElement.scrollTop;
 }
 
 class Bubble{
     constructor(){
         this.element = document.createElement("span");
         this.element.className = "circle";
-        this.element.style.top = "-150px";
-        this.realTop = -150;
         this.realLeft = Math.random()*window.screen.width;
         this.element.style.left = (Math.floor(this.realLeft)).toString() + "px";
         this.speed = 0.4 + Math.random()*0.8;
@@ -38,6 +68,8 @@ class Bubble{
         this.fadeSpeed = 0.008;
         this.alpha = 0.3;
         let size = Math.floor(15+ Math.random()*50);
+        this.realTop = -size;
+        this.top = this.realTop.toString() + "px";
         this.element.style.height = size.toString() + "px";
         this.element.style.width = size.toString() + "px";
         this.wobbleMagnitude = Math.random()*10 + 5;
@@ -79,6 +111,14 @@ class Bubble{
                 document.body.removeChild(this.element);
             }
         }
+    }
+
+    getX(){
+        return parseInt(window.getComputedStyle(this.element).left)+ parseInt(window.getComputedStyle(this.element).width)/2;
+    }
+
+    getY(){
+        return parseInt(window.getComputedStyle(this.element).top)+ parseInt(window.getComputedStyle(this.element).height)/2;
     }
 }
 
